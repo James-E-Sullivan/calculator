ca01af98d629b592c957d44d8717901fca7892ac
James Sullivan <jesullivan222@gmail.com>
2022-11-01 19:13:25 -0400

    Create add, subtract, multiply, and divided fxns

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// arithmetic functions
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (a === 0 || b === 0) return "DIVIDE BY ZERO ERROR";
+    return a / b;
+}
