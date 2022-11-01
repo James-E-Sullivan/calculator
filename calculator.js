575f04732f3b774e04684fd6d46aa7e40b81f90b
James Sullivan <jesullivan222@gmail.com>
2022-11-01 19:32:22 -0400

    Create tests for add function
    * Add json files for jest

--- calculator.js
+++ calculator.js
@@ -16,3 +16,11 @@ function divide(a, b) {
     if (a === 0 || b === 0) return "DIVIDE BY ZERO ERROR";
     return a / b;
 }
+
+// for testing - do not touch
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide
+};
--- calculator.spec.js
+++ calculator.spec.js
@@ -1,1 +1,19 @@
 const calculator = require('./calculator');
+
+describe('add', () => {
+    test('adds 0 and 0', () => {
+        expect(calculator.add(0,0)).toBe(0);
+    });
+
+    test('adds 2 and 2', () => {
+       expect(calculator.add(2,2)).toBe(4); 
+    });
+
+    test('adds positive numbers', () => {
+        expect(calculator.add(2,6)).toBe(8);
+    });
+
+    test('adds negative numbers', () => {
+        expect(calculator.add(-3, -1)).toBe(-4);
+    });
+});
