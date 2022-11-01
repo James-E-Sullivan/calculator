ef692e80acc56361f37963d1518ae0e1c1347258
James Sullivan <jesullivan222@gmail.com>
2022-11-01 19:47:29 -0400

    Add tests for subtract, multiply, and divide

--- calculator.spec.js
+++ calculator.spec.js
@@ -17,3 +17,45 @@ describe('add', () => {
         expect(calculator.add(-3, -1)).toBe(-4);
     });
 });
+
+describe('add', () => {
+    test('subtracts numbers', () => {
+        expect(calculator.subtract(10,4)).toBe(6);
+    });
+
+    test('subtracts negative numbers', () => {
+        expect(calculator.subtract(-3,-1)).toBe(-2);
+    });
+});
+
+describe('multiply', () => {
+    test('multiply two positive numbers', () => {
+        expect(calculator.multiply(10,4)).toBe(40);
+    });
+
+    test('multiply one negative and one positive number', () => {
+        expect(calculator.multiply(4, -3)).toBe(-12);
+    });
+
+    test('multiply two negative numbers', () => {
+        expect(calculator.multiply(-2, -3)).toBe(6);
+    });
+});
+
+describe('divide', () => {
+    test('divide two positive integers', () => {
+        expect(calculator.divide(16,4)).toBe(4);
+    });
+
+    test('divide one positive and one negative integer', () => {
+        expect(calculator.divide(-9,3)).toBe(-3);
+    });
+
+    test('divide two negative integers', () => {
+        expect(calculator.divide(-20, -5)).toBe(4);
+    });
+
+    test('divide decimal numbers', () => {
+        expect(calculator.divide(16.5, 8)).toBe(2.0625);
+    });
+});
