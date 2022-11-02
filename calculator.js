cde470888c68b3a2eddb2d53ebecec190fb8a5af
James Sullivan <jesullivan222@gmail.com>
2022-11-01 20:39:23 -0400

    Begin creation of display function methods
    * Begin creation of data structures/classes used to store user input
      prior to evaluation

--- calculator.js
+++ calculator.js
@@ -1,5 +1,4 @@
 // arithmetic functions
-
 function add(a, b) {
     if (isNaN(a) || isNaN(b)) return "ERROR: NOT A NUMBER";
     return a + b;
@@ -31,6 +30,37 @@ function operate(operator, a, b) {
             return multiply(a,b);
         case "divide":
             return divide(a,b);
+    }
+}
+
+
+// display functions
+function getNumberInput() {
+    const numberButtons = document.querySelectorAll('.num-btn');
+
+    numberButtons.forEach((numBtn) => {
+        numBtn.addEventListener('click', () => {
+            
+        });
+    });
+}
+
+function addToOperationQueue(input) {
+    // to be added ... will add btn input to a queue
+}
+
+// array used to store inputs prior to evaluation
+const operationQueue = [];
+
+class Operation {
+    constructor(operator, operandA, operandB) {
+        this.operator = operator;
+        this.operandA = operandA;
+        this.operandB = operandB;
+    }
+
+    operateExpression() {
+        return operate(operator, this.operandA, this.operandB);
     }
 }
 
